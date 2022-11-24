2bdca37a5de827ea4d2525be40c7a8e048224079
rcosta <regivaldocosta@yahoo.com.br>
2022-11-24 10:11:37 -0300

    Update constants.js

--- lambda/constants.js
+++ lambda/constants.js
@@ -1,20 +1,22 @@
 exports.audioData = [
-];
-
-/** Example format
-exports.audioData = [
+{
+    title: 'Melhor da Old School Music',
+    url: 'https://rcaudio.rclabs.com.br/madonna.mp3',
+  },    
   {
-    title: 'Title',
-    url: 'https://domain.com/test.mp3',
+    title: 'Programação ao Vivo',
+    url: 'https://rcaudio.rclabs.com.br/dreams1.mp3',
   },
   {
-    title: 'Title 2',
-    url: 'https://domain.com/test2.mp3',
+    title: 'Programa Esquina do Jazz',
+    url: 'https://rcaudio.rclabs.com.br/janet.mp3',
   },
   {
-    title: 'Title 3',
-    url: 'https://domain.com/test3.mp3',
+    title: 'Programa Kalimba',
+    url: 'https://rcaudio.rclabs.com.br/madonna.mp3',
+  },
+  {
+    title: 'Programa Roda de Choro',
+    url: 'https://rcaudio.rclabs.com.br/dreams2.mp3',
   }
 ];
-
-**/
